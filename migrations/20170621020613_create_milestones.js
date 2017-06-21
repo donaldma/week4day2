3a3d8e655e7614286fea576db764bef94fd30818
donaldma <donald.ma@hotmail.ca>
2017-06-21 03:38:39 +0000

    Added second migrations

--- migrations/20170621020613_create_milestones.js
+++ migrations/20170621020613_create_milestones.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable("milestones", (table) => {
-    table.increments();
+    table.increments('id').primary;
     table.string("description");
     table.date("date_achieved");
   });
